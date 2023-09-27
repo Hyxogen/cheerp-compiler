494f47cc0a98cbe4db446340f8d944a4d44004e7
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2023-09-27 11:58:03 +0200

    add tests

---
+++ compiler-rt/lib/test/heap-overflow.cpp
@@ -0,0 +1,17 @@
+// RUN: %clangxx_asan -O0 %s -o %t && not %run %t 2>&1 | FileCheck %s
+// RUN: %clangxx_asan -O1 %s -o %t && not %run %t 2>&1 | FileCheck %s
+// RUN: %clangxx_asan -O2 %s -o %t && not %run %t 2>&1 | FileCheck %s
+// RUN: %clangxx_asan -O3 %s -o %t && not %run %t 2>&1 | FileCheck %s
+
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+  char *x = (char*)malloc(10 * sizeof(char));
+  //memset(x, 0, 10);
+  int res = x[(argc + 1) * 10];  // BOOOM
+  // CHECK: {{READ of size 1 at 0x.*}}
+  free(x);
+  return res;
+}
+
---
+++ compiler-rt/lib/test/lit.cfg.py
@@ -0,0 +1,32 @@
+# -*- Python -*-
+
+import os
+import platform
+import re
+import lit.formats
+config.name = 'AddressSanitizer'
+
+cheerp_dest=os.environ['CHEERP_DEST']
+libasan_dir=os.environ['CHEERP_LIBASAN_DIR']
+
+config.suffixes = ['.c', '.cpp']
+config.test_format = lit.formats.ShTest("0")
+config.clang = cheerp_dest + "/bin/clang"
+
+def build_invocation(compile_flags):
+  return " " + " ".join([config.clang] + compile_flags) + " "
+
+extra_link_flags = []
+target_cflags = ["-target cheerp-wasm"] + extra_link_flags
+target_cxxflags = target_cflags + extra_link_flags
+clang_asan_static_cflags = (["-fsanitize=address", libasan_dir + "/libasan.bc", "-cheerp-linear-heap-size=2048"])
+clang_asan_static_cxxflags = clang_asan_static_cflags
+
+clang_asan_cflags = clang_asan_static_cflags
+clang_asan_cxxflags = clang_asan_static_cxxflags
+
+config.substitutions.append( ("%clang ", build_invocation(target_cflags)) )
+config.substitutions.append( ("%clangxx ", build_invocation(target_cxxflags)) )
+config.substitutions.append( ("%clang_asan ", build_invocation(clang_asan_cflags)) )
+config.substitutions.append( ("%clangxx_asan ", build_invocation(clang_asan_cxxflags)) )
+config.substitutions.insert(0, ('%run', "node"))
