fcd382e2e0e1e96a611214fb4b2da48823d2f2f8
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2023-10-03 17:41:54 +0200

    add test for calloc overflow

---
+++ compiler-rt/lib/test/calloc-overflow.cpp
@@ -0,0 +1,18 @@
+// RUN: %clangxx_asan -O0 %s -o %t && not %run %t 2>&1 | FileCheck %s
+// RUN: %clangxx_asan -cheerp-linear-output=asmjs -O0 %s -o %t && not %run %t 2>&1 | FileCheck %s
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  void *p = calloc(-1, 1000);
+  // CHECK: {{ERROR: AddressSanitizer: calloc parameters overflow: count \* size \(.* \* 1000\) cannot be represented in type size_t}}
+  // DONTCHECK: {{#0 0x.* in .*calloc}}
+  // DONTCHECK: {{#1 0x.* in main .*calloc-overflow.cpp:}}[[@LINE-3]]
+  // CHECK: SUMMARY: AddressSanitizer: calloc-overflow
+
+  //printf("calloc returned: %zu\n", (size_t)p);
+  // DONTCHECK-NULL: calloc returned: 0
+
+  return 0;
+}
