a796443e073f19c0334a49eb33e05c94f2759d09
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2023-11-07 13:53:13 +0100

    rectify old comment

--- compiler-rt/lib/asan/asan_shadow_setup.cpp
+++ compiler-rt/lib/asan/asan_shadow_setup.cpp
@@ -13,8 +13,7 @@
 
 #include "sanitizer_common/sanitizer_platform.h"
 
-// asan_fuchsia.cpp and asan_cheerpwasm.cpp have their own
-// InitializeShadowMemory implementation.
+// asan_fuchsia.cpp has their own InitializeShadowMemory implementation.
 #if !SANITIZER_FUCHSIA
 
 #  include "asan_internal.h"
