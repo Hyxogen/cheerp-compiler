e859fc71572d85f8766a35febb2ea1aaecf3c97d
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2023-11-15 17:43:12 +0100

    make it possible to run asan tests through cmake

--- compiler-rt/test/asan/lit.cfg.py
+++ compiler-rt/test/asan/lit.cfg.py
@@ -239,7 +239,7 @@ else:
   config.substitutions.append(('%pie', '-pie'))
 
 # Only run the tests on supported OSs.
-if config.host_os not in ['Linux', 'Darwin', 'FreeBSD', 'SunOS', 'Windows', 'NetBSD']:
+if config.host_os not in ['Linux', 'Darwin', 'FreeBSD', 'SunOS', 'Windows', 'NetBSD', "Cheerp"]:
   config.unsupported = True
 
 if not config.parallelism_group:
