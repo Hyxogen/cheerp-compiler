d8ea18a6e3bf6efc7fcd940ac71d146fcfe3c5f3
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2023-11-30 16:10:45 +0100

    asan: revert changes to unsupported test

--- compiler-rt/test/asan/TestCases/memcmp_test.cpp
+++ compiler-rt/test/asan/TestCases/memcmp_test.cpp
@@ -12,7 +12,6 @@
 // UNSUPPORTED: cheerp
 #include <string.h>
 int main(int argc, char **argv) {
-  ++argc;
   char a1[] = {static_cast<char>(argc), 2, 3, 4};
   char a2[] = {1, static_cast<char>(2*argc), 3, 4};
   int res = memcmp(a1, a2, 4 + argc);  // BOOM
