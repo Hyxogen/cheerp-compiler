e5e9182a92210fad4d80ecbd6d296add64f5ac78
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2023-12-28 09:16:21 +0100

    asan: fix stacktraces

--- compiler-rt/lib/sanitizer_common/sanitizer_stacktrace_cheerpwasm.cpp
+++ compiler-rt/lib/sanitizer_common/sanitizer_stacktrace_cheerpwasm.cpp
@@ -228,7 +228,7 @@ void BufferedStackTrace::UnwindFast(uptr pc, uptr bp, uptr stack_top,
   return 0;
 }
 
-uptr StackTrace::GetCurrentPc() { return GetReturnAddress(0); }
+[[clang::always_inline]] uptr StackTrace::GetCurrentPc() { return GetReturnAddress(0); }
 
 }  // namespace __sanitizer
 
